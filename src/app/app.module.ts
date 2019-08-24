f9fe95bc656d877512a579ddae4edb5d5fa3a88a
hamlet10 <hpsanchez@live.com>
2019-08-24 15:49:44 -0400

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { FotosComponent } from "./components/fotos/fotos.component";
+import { CargaComponent } from "./components/carga/carga.component";
+
+import { APP_ROUTES } from "./app.routes";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, FotosComponent, CargaComponent],
+  imports: [BrowserModule, APP_ROUTES],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
