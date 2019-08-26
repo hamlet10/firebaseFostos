66f5f8945d4f228b0c80c8813fa2d27b27c20833
hamlet10 <hpsanchez@live.com>
2019-08-26 15:50:09 -0400

    Almost done

--- src/app/directives/ng-drop-files.directive.ts
+++ src/app/directives/ng-drop-files.directive.ts
@@ -19,6 +19,7 @@ export class NgDropFilesDirective {
   @HostListener("dragover", ["$event"])
   public onDragEnter(event: any) {
     this.mouseSobre.emit(true);
+    this._prevenirDeter(event);
   }
 
   @HostListener("dragleave", ["$event"])
@@ -26,18 +27,66 @@ export class NgDropFilesDirective {
     this.mouseSobre.emit(false);
   }
 
-  //Validaciones
-  private _prevenirDeter(event) {
-    event.preventDefaul();
-    event.StopPropagation();
+  @HostListener("drop", ["$event"])
+  public onDrop(event: any) {
+    const transferencia = this._getTransferencia(event);
+
+    if (!transferencia) {
+      return;
+    }
+    this._extraerArchivos(transferencia.files);
+    this._prevenirDeter(event);
+    this.mouseSobre.emit(false);
   }
 
-  private _arachivoYaFueDroppeado(nombreArchivo: string): boolean {
+  private _getTransferencia(event: any) {
+    return event.dataTransfer
+      ? event.dataTransfer
+      : event.originalEvent.dataTransfer;
+  }
+
+  private _extraerArchivos(archivosLista: FileList) {
+    // console.log(archivosLista);
+    for (const propiedad in Object.getOwnPropertyNames(archivosLista)) {
+      const archivoTemporal = archivosLista[propiedad];
+      if (this._archivoPuedeSerCargado(archivoTemporal)) {
+        const nuevoArchivo = new FileItem(archivoTemporal);
+        this.archivos.push(nuevoArchivo);
+      }
+    }
+    console.log(this.archivos);
+  }
+  //Validaciones
+
+  private _archivoPuedeSerCargado(archivo: File): boolean {
+    if (
+      !this._archivoYaFueDroppeado(archivo.name) &&
+      this._esImagen(archivo.type)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  private _prevenirDeter(event) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  private _archivoYaFueDroppeado(nombreArchivo: string): boolean {
     for (const archivo of this.archivos) {
       if (archivo.nombreArchivo === nombreArchivo) {
         console.log("el archivo" + nombreArchivo + "ya fue agregado");
         return true;
       }
     }
+    return false;
+  }
+
+  private _esImagen(tipoArchivo: string): boolean {
+    return tipoArchivo === "" || tipoArchivo === undefined
+      ? false
+      : tipoArchivo.startsWith("image");
   }
 }
